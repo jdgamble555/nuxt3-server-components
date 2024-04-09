5edda3993c5a131c9244ad8cb87cade7e7e6a545
Jonathan Gamble <jdgamble555@gmail.com>
2024-04-08 20:28:58 -0500

    manifest fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nitro-cloudflare-dev"],
   experimental: {
-    componentIslands: true
+    componentIslands: true,
+    appManifest: false
   },
   runtimeConfig: {
     public: {
